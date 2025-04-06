05976a5e6de5d174a024fda5c720dd694ff22099
Longhanhmid24 <vophilong069@gmail.com>
2025-04-06 16:03:26 +0700

    WebDuLichLo

--- Back_End/WebDuLich/obj/Debug/net9.0/WebDuLich.AssemblyInfo.cs
+++ Back_End/WebDuLich/obj/Debug/net9.0/WebDuLich.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDuLich")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34fbf39fd4a1f0dbb3e4b73ccfba8355c55ce5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8980a9e057596c27808421a423ecb87e43fd3e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDuLich")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDuLich")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
