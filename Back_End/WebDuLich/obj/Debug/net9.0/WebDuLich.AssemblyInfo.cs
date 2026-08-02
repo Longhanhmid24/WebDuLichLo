50719d778470168f030268add98efd613054dbad
Vo Phi Long <vophilong069@gmail.com>
2026-08-02 14:38:17 +0700

    update backend

--- Back_End/WebDuLich/obj/Debug/net9.0/WebDuLich.AssemblyInfo.cs
+++ Back_End/WebDuLich/obj/Debug/net9.0/WebDuLich.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebDuLich")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbe123ebf9734d3753afd261de32859fb8975a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faf24d7701ebc66d91de4083a6a8ba32632394f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebDuLich")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebDuLich")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
